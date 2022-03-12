f489b7d9f40fa1e33584c73808a857876c3292b6
Dillon Estrada <dev@dillonestrada.com>
2022-03-12 16:49:44 -0600

    Get plot working

--- main.py
+++ main.py
@@ -2,31 +2,59 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import gspread
 
+gc = gspread.service_account()
 
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
-plt.rcParams["figure.autolayout"] = True
+sh = gc.open("Measurements Over Time")
 
-headers = ['1','2','3', '4', '5', '6', '7', '8', '9', '10']
-df = pd.read_csv('/home/dillonestrada/Documents/Development/measurements/SampleCSVFile.csv', encoding='unicode_escape')
+# print(sh.sheet1.get('A1'))
+
+df = pd.DataFrame(sh.get_worksheet(0).get_all_records())
+
+df['Date'] = pd.to_datetime(df['Date'])
+
+df.set_index('Date', inplace=True)
+
+# print(df.keys())
+
+# table = df.pivot(index='Date', columns=df.keys()[1::])
+
+# table.head()
+
+# table.plot()
+
+# print(table)
+
+# # dates = df['Date']
+
 print(df)
 
 df.plot()
-
 plt.show()
 
-# # X axis parameter:
-# xaxis = np.array([2, 8])
 
-# # Y axis parameter:
-# yaxis = np.array([4, 9])
+# plt.rcParams["figure.figsize"] = [7.50, 3.50]
 
-# plt.plot(xaxis, yaxis)
+# df = pd.read_csv(url_1, encoding='unicode_escape', header=1)
+# print(df)
+
+# df.plot()
+
+# plt.show()
+
+# # # X axis parameter:
+# # xaxis = np.array([2, 8])
+
+# # # Y axis parameter:
+# # yaxis = np.array([4, 9])
+
+# # plt.plot(xaxis, yaxis)
 
 
-# x = df['1']
-# y = df['2']
+# # x = df['1']
+# # y = df['2']
 
-# # plot
-# plt.plot(x,y)
-# plt.savefig("mygraph.png")
+# # # plot
+# # plt.plot(x,y)
+# # plt.savefig("mygraph.png")
